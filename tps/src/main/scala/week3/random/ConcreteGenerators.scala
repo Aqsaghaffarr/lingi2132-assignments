3ab27d32d01fbd2572907cc0970ac39e37a8a006
Gilles Peiffer <gilles.peiffer@outlook.com>
2020-04-22 17:51:30 +0200

    TP3: Add solutions

--- tps/src/main/scala/week3/random/ConcreteGenerators.scala
+++ tps/src/main/scala/week3/random/ConcreteGenerators.scala
@@ -1,9 +1,11 @@
 package week3.random
 
+import scala.collection.mutable.ListBuffer
+
 object ConcreteGenerators {
 
   val integers = new Generator[Int]() {
-    val rand = new scala.util.Random()
+    val rand = new scala.util.Random(0)
     def generate = rand.nextInt()
   }
 
@@ -12,26 +14,53 @@ object ConcreteGenerators {
 
   // TODO: write a generator for random pairs of Int's (hint, do not extend Generator, use for loops).
   // Try to formulate the same using only map/flatMaps)
-  val pairs: Generator[(Int, Int)] = ???
+  val pairs: Generator[(Int, Int)] = for (i <- integers; j <- integers) yield (i, j)
 
   // TODO create a generator of uniform random numbers between lo and hi (not included) (hint: use only for loops)
-  def choose(lo: Int, hi: Int): Generator[Int] = ???
+  def choose(lo: Int, hi: Int): Generator[Int] = for (i <- integers) yield Math.abs(i) % (hi - lo) + lo
 
   // TODO create a generator selecting randomly among a number of xs (hint: use only for loops)
-  def oneFrom[T](xs: T*): Generator[T] = ???
+  def oneFrom[T](xs: T*): Generator[T] = for (i <- choose(0, xs.length)) yield xs(i)
 
   // A generator that always generate the same value x
   def single[T](x: T): Generator[T] = new Generator[T] {
-    def generate = x
+    def generate: T = x
   }
 
   // TODO: create a generator generating empty lists (hint: user single)
-  def emptyLists = ???
+  def emptyLists: Generator[List[Nothing]] = single(List())
 
   // TODO:: create a generator generating random (non empty) lists of Int's (use for loops)
-  def nonEmptyLists: Generator[List[Int]] = ???
+  def nonEmptyLists: Generator[List[Int]] = new Generator[List[Int]] {
+    override def generate: List[Int] = {
+      val l = ListBuffer(integers.generate)
+      while (booleans.generate)
+        l :+ integers.generate
+      l.toList
+    }
+  }
 
   // TODO:: create a generator generating random lists, (empty or not empty)
-  def lists: Generator[List[Int]] = ???
+  def lists: Generator[List[Int]] = new Generator[List[Int]] {
+    override def generate: List[Int] = if (booleans.generate) nonEmptyLists.generate else emptyLists.generate
+  }
 
+  // Compare map to flatMap
+  def tst: Boolean = {
+    val g1 = new Generator[List[Int]]() {
+      def generate = List(1, 2)
+    }
+    val g2 = new Generator[List[Int]]() {
+      def generate = List(1, 2)
+    }
+    val a = g1.map(x => (x, x))
+    val b = g2.flatMap(x => g2.unit((x, x)))
+    var c = true
+    for (_ <- 0 to 100) {
+      val x = a.generate
+      val y = b.generate
+      c &= x == y
+    }
+    c
+  }
 }
--- tps/src/main/scala/week3/random/Generator.scala
+++ tps/src/main/scala/week3/random/Generator.scala
@@ -10,17 +10,34 @@ trait Generator[T] {
    */
   def generate: T
 
+  def unit[T](x: T): Generator[T] = {
+    new Generator[T] {
+      def generate: T = x
+    }
+  }
+
   def map[S](f: T => S): Generator[S] = {
     new Generator[S] {
-      def generate = f(Generator.this.generate)
+      def generate: S = f(Generator.this.generate)
     }
   }
   
-  def flatMap[S](f: T => Generator[S]):  Generator[S] = {
+  def flatMap[S](f: T => Generator[S]): Generator[S] = {
     new Generator[S] {
       // TODO: complete the implementation of flatMap
-      def generate = ???
+      def generate: S = f(Generator.this.generate).generate
     }    
+  }
+
+  def withFilter(f: T => Boolean): Generator[T] = {
+    new Generator[T] {
+      def generate: T = {
+        var sol = this.generate
+        while (!f(sol))
+          sol = this.generate
+        sol
+      }
+    }
   }
 }
 
